7f47fb430df8118cab77b03ba3530c78a46b16c5
stars <a914ca@gmail.com>
2025-10-21 18:39:54 +0200

    Create BitcoinKitVersion.swift

---
+++ Sources/Utils/BitcoinKitVersion.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinKitVersion.swift
+//  BitcoinKit
+//
+//  Created by Contributor on 2025/10/18.
+//
+
+import Foundation
+
+/// Prints the current version of BitcoinKit framework.
+public struct BitcoinKitVersion {
+    public static let version = "1.1.0"
+
+    public static func printVersion() {
+        print("BitcoinKit version: \(version)")
+    }
+}
